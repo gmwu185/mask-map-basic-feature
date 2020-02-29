dbcd4024721c31f7400e8bfa565989e0aa311753
gmwu185 <gmwu185@gmail.com>
2020-03-01 00:34:53 +0800

    透過新變數取得資料中的陣列資料

--- all.js
+++ all.js
@@ -2,3 +2,7 @@ var _date = '星期三';
 
 document.querySelector('.js-week').innerHTML = _date;
 // document.querySelector('.js-week').textContent = _date;
+
+console.log(data);
+var data2 = data.features
+console.log(data2);
